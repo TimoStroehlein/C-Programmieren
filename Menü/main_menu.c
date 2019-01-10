54397752ff9ddcb4afd86a4e4de8a20e3192860b
Timo Ströhlein <timostroehlein@gmail.com>
2019-01-10 21:24:48 +0100

    10.01.19

--- Menü/main_menu.c
+++ Menü/main_menu.c
@@ -1,90 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "spiel_hinzufuegen.c"
 
-void spieleAnzeigen();
-void spielHinzufuegen();
-
-struct m_spiel
-{
-  char name[30+1];
-  char genre[30+1];
-  int preis;
-  char veroeffentlichung[10+1];
-};
-
-void main ()
+void main (void)
 {
   int eingabe;
-
-  printf("Hauptmen\201\n");
-  printf("--------------------------------------\n");
-  printf("[1] Spiele Anzeigen\n");
-  printf("[2] Spiele Hinzuf\201gen\n");
-  printf("[3] Funktion 3\n");
-  printf("[4] Funktion 4\n");
-  printf("[5] Funktion 5\n");
-  printf("--------------------------------------\n");
-  printf("[0] Beenden\n");
-
-  scanf("%i", &eingabe);
-  fflush(stdin);
-
-  switch (eingabe)
+  while (eingabe != 0)
   {
-    case 0:
-      printf("Beendet\n");
-      break;
-    case 1:
-      printf("Funktion 1 wurde ausgef\201hrt\n");
-      break;
-    case 2:
-      printf("Funktion 2 wurde ausgef\201hrt\n");
-      spielHinzufuegen();
-      break;
-    case 3:
-      printf("Funktion 3 wurde ausgef\201hrt\n");
-      break;
-    case 4:
-      printf("Funktion 4 wurde ausgef\201hrt\n");
-      break;
-    case 5:
-      printf("Funktion 5 wurde ausgef\201hrt\n");
-      break;
-    default:
-      printf("Zahl nicht gefunden.\n");
-      break;
+    system("cls");
+
+    printf("Hauptmen\201\n");
+    printf("--------------------------------------\n");
+    printf("[1] Spiele Anzeigen\n");
+    printf("[2] Spiele Hinzuf\201gen\n");
+    printf("[3] Funktion 3\n");
+    printf("[4] Funktion 4\n");
+    printf("[5] Funktion 5\n");
+    printf("--------------------------------------\n");
+    printf("[0] Beenden\n");
+
+    scanf("%i", &eingabe);
+    fflush(stdin);
+
+    switch (eingabe)
+    {
+      case 1:
+        system("cls");
+        printf("Funktion 1 wurde ausgef\201hrt\n");
+        liste_lesen();
+        break;
+      case 2:
+        system("cls");
+        printf("Funktion 2 wurde ausgef\201hrt\n");
+        break;
+      case 3:
+        system("cls");
+        printf("Funktion 3 wurde ausgef\201hrt\n");
+        break;
+      case 4:
+        system("cls");
+        printf("Funktion 4 wurde ausgef\201hrt\n");
+        break;
+      case 5:
+        system("cls");
+        printf("Funktion 5 wurde ausgef\201hrt\n");
+        break;
+      case 0:
+        system("cls");
+        printf("Programm Beendet.\n");
+        break;
+      default:
+        system("cls");
+        printf("Zahl nicht gefunden.\n");
+        printf("Programm Beendet.\n");
+        break;
+    }
   }
-}
-
-void spieleAnzeigen ()
-{
-
-}
-
-void spielHinzufuegen ()
-{
-  char cname[30+1];
-  char cgenre[30+1];
-  int cpreis;
-  char cveroeffentlichung[10+1];
-
-  printf("Name: ");
-  scanf("%s", cname);
-  fflush(stdin);
-  printf("Genre: ");
-  scanf("%s", cgenre);
-  fflush(stdin);
-  printf("Preis: ");
-  scanf("%i", &cpreis);
-  fflush(stdin);
-  printf("Ver\201ffentlichung: ");
-  scanf("%s", cveroeffentlichung);
-  fflush(stdin);
-
-}
-
-void hauptmenue ()
-{
-
 }
--- Menü/spiel_hinzufuegen.c
+++ Menü/spiel_hinzufuegen.c
@@ -2,81 +2,119 @@
 #include <string.h>
 #include <stdlib.h>
 
-void spiel_hinzufuegen();
+void liste_lesen();
+void liste_add();
 void struct_liste();
-void lesen();
-void schreiben();
+void datei_lesen();
+void datei_schreiben();
+void debug();
 
 typedef struct m_spiel
 {
   char name[20+1];
   char genre[20+1];
-  char preis[20+1];
+  int preis;
   char datum[10+1];
 
   struct m_spiel *davor;
   struct m_spiel *danach;
 } t_spiel;
+
 typedef struct
 {
   char name[20+1];
   char genre[20+1];
-  char preis[20+1];
+  int preis;
   char datum[10+1];
 
-  t_spiel *mom, *start, *zwischen;
+  t_spiel *mom, *erster, *letzter;
 } t_feld;
 
-void main (void)
+void liste_lesen (void)
 {
+  int temp;
   t_feld feld;
   t_feld *f = &feld;
   f -> mom = 0;
-  f -> start = 0;
-  f -> zwischen = 0;
+  f -> erster = 0;
+  f -> letzter = 0;
 
   printf("Name: ");
   scanf("%s", f -> name);
   fflush(stdin);
+
   printf("Genre: ");
   scanf("%s", f -> genre);
   fflush(stdin);
+
   printf("Preis: ");
-  scanf("%s", f -> preis);
+  scanf("%i", &(f -> preis));
   fflush(stdin);
+
   printf("Datum: ");
   scanf("%s", f -> datum);
   fflush(stdin);
-  spiel_hinzufuegen();
-  lesen();
+
+  liste_add(f);
+
+  //debug(f);
+  datei_lesen(f);
+  //datei_schreiben(f);
 }
 
-void spiel_hinzufuegen (t_feld *f)
+void liste_add (t_feld *f)
 {
-  f -> mom = (t_spiel*)malloc(sizeof(t_spiel));  //Malloc liefert void Zeiger
-  struct_liste(f);
+  f -> mom = (t_spiel*)malloc(sizeof(t_spiel));  //Reserviert Größe des structs im Hauptspeicher, Typecast nötig, da return value von malloc Void-Zeiger ist
+  struct_liste(f);  //Kopiert eingelesenes in die Liste
 
-  f -> mom -> davor = f -> zwischen;
+  f -> mom -> davor = f -> letzter;
   f -> mom -> danach = 0;
 
-  if (f -> zwischen == 0) f -> start = f -> mom;
-  else f -> zwischen -> danach = f-> mom;
+  if (f -> letzter == 0) f -> erster = f -> mom;
+  else f -> letzter -> danach = f-> mom;
 
-  f -> zwischen = f -> mom;
+  f -> letzter = f -> mom;
 }
 
 void struct_liste (t_feld *f)
 {
   strcpy(f -> mom -> name, f -> name);
+  strcpy(f -> mom -> genre, f -> genre);
+  f -> mom -> preis = f -> preis;
+  strcpy(f -> mom -> datum, f -> datum);
 }
 
-void schreiben (void)
+void debug (t_feld *f)
+{
+  f -> mom = f -> erster;
+
+  while (f -> mom)
+  {
+    printf("\n%s %s %s %s %x %x %x",
+      f -> mom -> name,
+      f -> mom -> genre,
+      f -> mom -> preis,
+      f -> mom -> datum,
+      f -> mom,
+      f -> mom -> davor,
+      f -> mom -> danach);
+
+    f -> mom = f -> mom -> danach;
+  }
+}
+
+void datei_schreiben (t_feld *f)
 {
   FILE *datei;
-  datei = fopen("spiele.txt", "w");
+  datei = fopen("spiele.txt", "a+");
+  fwrite(f -> mom -> name, 20, 1, datei);
+  fwrite(f -> mom -> genre, 20, 1, datei);
+  fwrite(&(f -> mom -> preis), 3, 1, datei);
+  fwrite(f -> mom -> datum, 10, 1, datei);
+  fclose(datei);
 }
 
-void lesen (t_feld *f)
+void datei_lesen (t_feld *f)
 {
   FILE *datei;
   char text[200+1];
@@ -90,18 +128,23 @@ void lesen (t_feld *f)
     fgets(text, 200, datei);
     while (!feof(datei))
     {
-      //printf("\n%s", text);
       fgets(text, 200, datei);
-      strncpy(f -> name, text, 20);
-      strncpy(f -> genre, text+20, 20);
-      strncpy(f -> preis, text+40, 20);
-      strncpy(f -> datum, text+60, 20);
-
-      printf("%s\n", f -> name);
-      printf("%s\n", f -> genre);
-      printf("%s\n", f -> preis);
-      printf("%s\n", f -> datum);
+      strncpy(f -> mom -> name, text, 20);
+      strncpy(f -> mom -> genre, text+20, 20);
+      char temp[3+1];
+      strncpy(temp, text+40, 3);
+      for (int i = 0; i < sizeof(temp); i++)
+      {
+        if ((int)temp[i] != 0)  f -> mom -> preis = (int)temp[i];
+      }
+      strncpy(f -> mom -> datum, text+43, 10);
+      printf("%s\n", f -> mom -> name);
+      printf("%s\n", f -> mom -> genre);
+      printf("%i\n", f -> mom -> preis);
+      printf("%s\n", f -> mom -> datum);
     }
-    fclose(datei);
   }
+  fclose(datei);
 }
+
+//Peter               Drama               56                  07.06.18
